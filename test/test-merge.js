4d7d0b78c083661e568b64237e0a92988389a4aa
Trevor Livingston <tlivings@gmail.com>
2019-03-02 21:27:55 -0600

    more merge test

--- test/test-merge.js
+++ test/test-merge.js
@@ -5,7 +5,7 @@ const Merge = require('../lib/merge');
 Test('merge', async (t) => {
 
   t.test('merge resolvers', (t) => {
-    t.plan(2);
+    t.plan(3);
 
     const resolvers = {
       Query: {
@@ -16,6 +16,9 @@ Test('merge', async (t) => {
     const merge = {
       Query: {
         merge: () => {}
+      },
+      Mutation: {
+        merge: () => {}
       }
     };
 
@@ -23,7 +26,16 @@ Test('merge', async (t) => {
 
     t.ok(merged.Query.test, 'original present');
     t.ok(merged.Query.merge, 'merged present');
+    t.ok(merged.Mutation.merge, 'merged present');
 
+  });
+
+  t.test('merge resolvers no args', (t) => {
+    t.plan(1);
+
+    const merged = Merge.mergeResolvers();
+
+    t.ok(merged, 'something was returned');
   });
 
 });
