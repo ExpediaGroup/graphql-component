e312c741007b98601ad04f3a5aea0d85659cade8
Abhishek Nath <anath@homeaway.com>
2019-07-02 14:17:56 -0700

    adding more tests for component-directive integration

--- test/test-directives.js
+++ test/test-directives.js
@@ -1,7 +1,48 @@
 'use strict';
 
 const Test = require('tape');
+
+const GraphQLComponent = require('../lib/index');
 const Directives = require('../lib/directives');
+
+Test('test component', (t) => {
+  t.test('component construct', async (t) => {
+    t.plan(6);
+
+    const componentA = new GraphQLComponent({
+      types: [`
+        type A {
+          value: String
+        }
+        type Query {
+          a: A
+        }
+      `],
+      directives: {constraint: 'constraint-directive'}
+    });
+    t.deepEquals(componentA.directives, {constraint: 'constraint-directive'});
+    t.deepEquals(componentA.importedDirectives, []);
+    t.deepEquals(componentA.mergedDirectives, {constraint: 'constraint-directive'});
+
+    const componentB = new GraphQLComponent({
+      types: [`
+        type B {
+          value: String
+        }
+        type Query {
+          b: B
+        }
+      `],
+      imports: [
+        componentA
+      ],
+      directives: {deprecated: 'deprecated-directive'}
+    });
+    t.deepEquals(componentB.directives, {deprecated: 'deprecated-directive'});
+    t.deepEquals(componentB.importedDirectives, [{constraint: 'constraint-directive'}]);
+    t.deepEquals(componentB.mergedDirectives, {constraint: 'constraint-directive', deprecated: 'deprecated-directive'});
+  });
+});
 
 Test('test directives utilities', (t) => {
   t.test('execute mergeDirectives', async (t) => {
