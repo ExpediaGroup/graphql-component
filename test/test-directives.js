dcdc5fa13c2f301ca10c4c770b5629afe0fee1fb
Abhishek Nath <anath@homeaway.com>
2019-07-05 11:48:18 -0700

    adding more tests to test directives in schema

--- test/test-directives.js
+++ test/test-directives.js
@@ -1,74 +1,163 @@
 'use strict';
 
 const Test = require('tape');
+const {SchemaDirectiveVisitor} = require('graphql-tools');
 
 const GraphQLComponent = require('../lib/index');
 const Directives = require('../lib/directives');
 
-Test('test component', (t) => {
-  t.test('component construct', async (t) => {
-    t.plan(6);
+class TestDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {}
+  visitEnumValue(field) {}
+}
 
-    const componentA = new GraphQLComponent({
-      types: [`
-        type A {
-          value: String
-        }
-        type Query {
-          a: A
-        }
-      `],
-      directives: {constraint: 'constraint-directive'}
-    });
-    t.deepEquals(componentA.directives, {constraint: 'constraint-directive'});
-    t.deepEquals(componentA.importedDirectives, []);
-    t.deepEquals(componentA.mergedDirectives, {constraint: 'constraint-directive'});
+const componentA = new GraphQLComponent({
+  types: [`
+      type Book {
+        id: ID!
+        title: String
+      }
+      type Query {
+        book(id: ID!) : Book
+      }
+    `],
+  resolvers: {
+    Query: {
+      book() {
+        return {id: 1, title: 'Some Title'}
+      }
+    }
+  },
+  directives: {constraint: TestDirective}
+});
 
-    const componentB = new GraphQLComponent({
-      types: [`
-        type B {
-          value: String
-        }
-        type Query {
-          b: B
-        }
-      `],
-      imports: [
-        componentA
-      ],
-      directives: {deprecated: 'deprecated-directive'}
+const componentB = new GraphQLComponent({
+  types: [`
+      type Author {
+        id: ID!
+        name: String
+        books: [Book]
+      }
+      type Query {
+        author(id: ID!) : Author
+      }
+    `],
+  resolvers: {
+    Query: {
+      author() {
+        return {
+          id: 1, name: 'Some Author',
+          books: [{id: 1, title: 'Some Title'}]
+        };
+      }
+    }
+  },
+  imports: [
+    componentA
+  ],
+  directives: {deprecated: TestDirective}
+});
+
+Test('test componentA', (t) => {
+  t.test('componentA construct', async (t) => {
+    t.plan(3);
+
+    t.deepEquals(componentA.directives, {constraint: TestDirective}, 'has constraint directive in directives');
+    t.deepEquals(componentA._importedDirectives, [], 'imported directives are empty');
+    t.deepEquals(componentA.mergedDirectives, {constraint: TestDirective}, 'has constraint directive in merged directives');
+  });
+
+  t.test('componentA schema', async (t) => {
+    t.plan(2);
+
+    t.ok(componentA.schema._directives, 'has directives');
+
+    const TestDirective = componentA.schema._directives.filter((d) => {
+      return d.name === "constraint";
     });
-    t.deepEquals(componentB.directives, {deprecated: 'deprecated-directive'});
-    t.deepEquals(componentB.importedDirectives, [{constraint: 'constraint-directive'}]);
-    t.deepEquals(componentB.mergedDirectives, {constraint: 'constraint-directive', deprecated: 'deprecated-directive'});
+    t.ok(TestDirective, 'has constraint directive in schema');
+  });
+
+  t.test('componentA execute', async (t) => {
+    t.plan(3);
+
+    const query = `
+      query {
+        book(id: 1) {id, title}
+      }
+    `;
+
+    const result = await componentA.execute(query);
+
+    t.ok(result, 'has result');
+    t.ok(result.data, 'data returned');
+    t.error(result.errors, 'no errors');
+  });
+});
+
+Test('test componentB', async (t) => {
+  t.test('componentB construct', async (t) => {
+    t.plan(3);
+
+    t.deepEquals(componentB.directives, {deprecated: TestDirective}, 'has deprecated directive in directives');
+    t.deepEquals(componentB._importedDirectives, [{constraint: TestDirective}], 'has constraint directive in imported directives');
+    t.deepEquals(componentB.mergedDirectives, {constraint: TestDirective, deprecated: TestDirective}, 'has constraint and deprecated directives in merged directives');
+  });
+
+  t.test('componentB schema', async (t) => {
+    t.plan(3);
+
+    t.ok(componentB.schema._directives, 'has directives');
+
+    const constraintDirective = componentB.schema._directives.filter((d) => {
+      return d.name === "constraint";
+    });
+    t.ok(constraintDirective, 'has constraint directive in schema');
+
+    const deprecatedDirective = componentB.schema._directives.filter((d) => {
+      return d.name === "deprecated";
+    });
+    t.ok(deprecatedDirective, 'has deprecated directive in schema');
+  });
+
+  t.test('componentB execute', async (t) => {
+    t.plan(3);
+
+    const query = `
+      query {
+        author(id: 1) {id, name, books}
+      }
+    `;
+
+    const result = await componentB.execute(query);
+
+    t.ok(result, 'has result');
+    t.ok(result.data, 'data returned');
+    t.error(result.errors, 'no errors');
   });
 });
 
 Test('test directives utilities', (t) => {
   t.test('execute mergeDirectives', async (t) => {
     t.plan(1);
-    const directives = [{constraint: 'constraint-directive'}, {deprecated: 'deprecated-directive'}];
-
+    const directives = [{constraint: TestDirective}, {deprecated: TestDirective}];
     const merged = Directives.mergeDirectives(directives);
 
-    t.deepEquals(merged, {
-      constraint: 'constraint-directive',
-      deprecated: 'deprecated-directive'
-    });
+    t.deepEquals(merged, {constraint: TestDirective, deprecated: TestDirective}, 'has constraint and deprecated directives in merged directives');
   });
 
   t.test('execute getImportedDirectives', async (t) => {
     t.plan(1);
     const component = {
-      _directives: {auth: 'auth-directive'},
-      _importedDirectives: [{constraint: 'constraint-directive'}, {deprecated: 'deprecated-directive'}]};
-
+      _directives: {auth: TestDirective},
+      _importedDirectives: [{constraint: TestDirective}, {deprecated: TestDirective}]
+    };
     const imported = Directives.getImportedDirectives(component);
 
     t.deepEquals(imported, {
-      auth: 'auth-directive',
-      constraint: 'constraint-directive',
-      deprecated: 'deprecated-directive'
-    });
+      auth: TestDirective,
+      constraint: TestDirective,
+      deprecated: TestDirective
+    }, 'has auth, constraint and deprecated directives in imported directives');
   });
 });
