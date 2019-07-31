b44877d588f7441d41a4ca3c7e149ae4d9d64d9c
Trevor Livingston <tlivings@gmail.com>
2019-07-31 18:49:23 -0500

    rename only when present

--- test/examples/example-listing/property-component/index.js
+++ test/examples/example-listing/property-component/index.js
@@ -4,10 +4,18 @@ const GraphQLComponent = require('../../../../lib/index');
 const Resolvers = require('./resolvers');
 const Types = require('./types');
 const Mocks = require('./mocks');
+const { SchemaDirectiveVisitor } = require('graphql-tools');
 
 class PropertyComponent extends GraphQLComponent {
   constructor({ useMocks, preserveTypeResolvers } = {}) {
-    super({ types: Types, resolvers: Resolvers, mocks: Mocks, useMocks, preserveTypeResolvers });
+    const directives = {
+      legacy: class extends SchemaDirectiveVisitor {
+        visitFieldDefinition(field, details) {
+          console.log('LEGACY');
+        }
+      }
+    }
+    super({ types: Types, resolvers: Resolvers, mocks: Mocks, useMocks, preserveTypeResolvers, directives });
   }
 }
 
