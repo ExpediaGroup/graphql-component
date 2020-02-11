7a01d91da626e6e8f249e5592809d01019564529
Trevor Livingston <tlivings@gmail.com>
2020-02-11 15:21:24 -0600

    test errorhandling delegate resolver'

--- test/test-delegate.js
+++ test/test-delegate.js
@@ -35,3 +35,46 @@ Test('component resolver delegate', async (t) => {
   
   t.equal(test.value, true, 'resolved');
 });
+
+Test('component resolver delegate errors', async (t) => {
+
+  t.plan(2);
+
+  const component = new GraphQLComponent({
+    imports: [
+      new GraphQLComponent({
+        types: `
+          type Test {
+            value: Boolean
+            err: Boolean
+          }
+          type Query {
+            test: Test
+          }
+        `,
+        resolvers: {
+          Query: {
+            test() {
+              return {
+                value: true,
+                err: true
+              }
+            }
+          },
+          Test: {
+            err(_) {
+              if (_.err) {
+                throw new Error('error');
+              }
+            }
+          }
+        }
+      })
+    ]
+  });
+
+  const { test } = await component.execute(`query { test { ...AllTest } }`, { mergeErrors: true });
+  
+  t.equal(test.value, true, 'resolved');
+  t.ok(test.err instanceof Error, 'got error');
+});
