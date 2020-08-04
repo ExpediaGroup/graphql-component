98b6fefdfc7ec657b709feb661e9893f4eb3ae2a
bquinn <bquinn@vrbo.com>
2020-08-04 16:39:18 -0700

    simplify import loop in constructor, replace transformResolvers with mergeResolvers exclusions functionality with tests for coverage, modify types import to support simpler import loop, pull in delegate implementation from 2.x with supporting tests

--- test/test-delegate.js
+++ test/test-delegate.js
@@ -2,10 +2,12 @@
 
 const Test = require('tape');
 const GraphQLComponent = require('../lib');
+const graphql = require('graphql');
+const gql = require('graphql-tag');
 
-Test('component resolver delegate', async (t) => {
+Test('automatic imported root resolver delegation', async (t) => {
 
-  t.plan(1);
+  t.plan(2);
 
   const component = new GraphQLComponent({
     imports: [
@@ -31,14 +33,26 @@ Test('component resolver delegate', async (t) => {
     ]
   });
 
-  const { test } = await component.execute(`query { test { ...AllTest } }`, { mergeErrors: true });
+  const document = gql`
+    query {
+      test { value }
+    }
+  `;
+
+  const { data, errors } = await graphql.execute({
+    document,
+    schema: component.schema,
+    rootValue: undefined,
+    contextValue: {}
+  });
   
-  t.equal(test.value, true, 'resolved');
+  t.equal(data.test.value, true, 'resolved');
+  t.notOk(errors, 'no errors');
 });
 
-Test('component resolver delegate errors', async (t) => {
+Test('automatic imported root resolver delegation with errors', async (t) => {
 
-  t.plan(2);
+  t.plan(4);
 
   const component = new GraphQLComponent({
     imports: [
@@ -73,8 +87,142 @@ Test('component resolver delegate errors', async (t) => {
     ]
   });
 
-  const { test } = await component.execute(`query { test { ...AllTest } }`, { mergeErrors: true });
+  const successDocument = gql`
+    query {
+      test { value }
+    }
+  `;
+
+  const errorDocument = gql`
+    query {
+      test {
+        value
+        err
+      }
+    }
+  `;
+
+  const successResult = await graphql.execute({
+    document: successDocument,
+    schema: component.schema,
+    rootValue: undefined,
+    contextValue: {}
+  });
   
-  t.equal(test.value, true, 'resolved');
-  t.ok(test.err instanceof Error, 'got error');
+  t.equal(successResult.data.test.value, true, 'resolved');
+  t.notOk(successResult.errors, 'no errors');
+
+  const errorResult = await graphql.execute({
+    document: errorDocument,
+    schema: component.schema,
+    rootValue: undefined,
+    contextValue: {}
+  });
+  
+  t.equal(errorResult.data.test.value, true, 'resolved');
+  t.equal(errorResult.errors[0].message, 'error', 'error propagated properly');
+});
+
+Test('automatic imported root resolver delegation with errors (return type not nullable)', async (t) => {
+
+  t.plan(2);
+
+  const component = new GraphQLComponent({
+    imports: [
+      new GraphQLComponent({
+        types: `
+          type Test {
+            value: Boolean
+          }
+          type Query {
+            test: Test!
+          }
+        `,
+        resolvers: {
+          Query: {
+            test() {
+              throw new Error('error in proxied root resolver');
+            }
+          }
+        }
+      })
+    ]
+  });
+
+  const document = gql`
+    query {
+      test {
+        value
+      }
+    }
+  `;
+
+  const {data, errors} = await graphql.execute({
+    document,
+    schema: component.schema,
+    rootValue: undefined,
+    contextValue: {}
+  });
+  
+  t.notOk(data, 'data is null');
+  t.equal(errors[0].message, 'error in proxied root resolver', 'error propagated properly');
+});
+
+Test('automatic root resolver delegation with abstract return type', async (t) => {
+  t.plan(3);
+
+  let resolveTypeCallCount = 0;
+  const component = new GraphQLComponent({
+    imports: [
+      new GraphQLComponent({
+        types: `
+          interface ITest {
+            id: ID
+          }
+          type Test implements ITest {
+            id: ID
+            value: Boolean
+          }
+          type Query {
+            test: ITest
+          }
+        `,
+        resolvers: {
+          Query: {
+            test() {
+              return { id: 1, value: true };
+            }
+          },
+          ITest: {
+            __resolveType(i) {
+              resolveTypeCallCount = resolveTypeCallCount + 1;
+              if (i.value) {
+                return 'Test'
+              }
+            }
+          }
+        }
+      })
+    ]
+  });
+
+  const document = gql`
+    query {
+      test {
+        id
+        value
+      }
+    }
+  `;
+
+  const {data, errors} = await graphql.execute({
+    document,
+    schema: component.schema,
+    rootValue: undefined,
+    contextValue: {}
+  });
+  
+  t.deepEqual(data.test, { id: '1', value: true});
+  t.equals(resolveTypeCallCount, 1, '__resolveType in import only called 1 time');
+  t.notOk(errors, 'no errors');
 });
